76d63a1cd6c5c90fbfdc711d3795915ffb01e68a
MJ Eng <mj.eng.contact@gmail.com>
2026-05-10 17:20:17 +0200

    Add ErrorController for centralized error handling
    Introduced ErrorController in RecipeSnap_BE.Controllers with a /error endpoint that returns a generic Problem response.

---
+++ RecipeSnap_BE/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeSnap_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult HandleError() => Problem();
+    }
+}
