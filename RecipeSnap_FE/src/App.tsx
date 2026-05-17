c7901d2e8eaf7fda625fce680b23e649dbb01cdc
MJ Eng <mj.eng.contact@gmail.com>
2026-05-17 03:54:31 +0200

    Refactor App layout and replace components
    Replaced `TestPage` with `HeroSection` and added `ScanApp`
    and `Instructions` components. Removed static "hero" and
    "next-steps" sections, including documentation and social
    links. Cleaned up unused code and imports.

--- RecipeSnap_FE/src/App.tsx
+++ RecipeSnap_FE/src/App.tsx
@@ -1,94 +1,26 @@
 import './App.css'
-import TestPage from './components/TestPage'
+import HeroSection from './components/layouts/HeroSection'
+import ScanApp from './components/ScanApp'
 import Footer from './components/layouts/Footer'
+import Instructions from './components/Instructions'
 
 function App() {
   return (
     <>
 
       <section id="center">
-        <div className="hero">
-          <h1>Welcome to RecipeSnap</h1>
-        </div>
-        <div>
-        </div>
-        <TestPage />
+        <HeroSection />
       </section>
 
-      <div className="ticks"></div>
-
       <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              VITE
-            </li>
-            <li>
-              React
-            </li>
-          </ul>
+        <div id="instructions">
+          <h2>Instructions</h2>
+          <Instructions />
+
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+        <div id="scan">
+          <h2 className='mb-3'>Try RecipeSnap</h2>
+          <ScanApp />
         </div>
       </section>
 
